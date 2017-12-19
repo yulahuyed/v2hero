f4d14c6a193cf873574b36d463609425dc4c11b0
yulahuyed <yulahuyed@vpsorg.top>
2017-12-19 13:32:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 cd /v2raybin
-echo -e -n "$CONFIG_JSON1" > config.json
-echo -e -n "$PORT" >> config.json
-echo -e -n "$CONFIG_JSON2" >> config.json
-echo -e -n "$UUID" >> config.json
-echo -e -n "$CONFIG_JSON3" >> config.json
+if [ "${CONFIG_URL}" ]
+then
+    curl -L -o config.json "${CONFIG_URL}"
+    sed -i 's/${OID}/${UUID}/g' config.json
+    sed -i 's/${OPORT}/${PORT}/g' config.json
+else
+    echo -e -n "$CONFIG_JSON1" > config.json
+    echo -e -n "$PORT" >> config.json
+    echo -e -n "$CONFIG_JSON2" >> config.json
+    echo -e -n "$UUID" >> config.json
+    echo -e -n "$CONFIG_JSON3" >> config.json
+fi
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
 echo -e "$CERT_PEM" > cert.pem
 echo -e "$KEY_PEM"  > key.pem
