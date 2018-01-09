0ca3de3bc79165491304dde01514ba33b45345bd
yulahuyed <yulahuyed@vpsorg.top>
2018-01-09 19:52:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@ cd /v2raybin
 if [ "${CONFIG_URL}" ]
 then
     curl -L -o config.json "${CONFIG_URL}"
-    sed -i 's/${OID}/${UUID}/g' config.json
-    sed -i 's/${OPORT}/$PORT/g' config.json
+    sed -i "s/${OID}/${UUID}/g" config.json
+    sed -i "s/${OPORT}/$PORT/g" config.json
 else
     echo -e -n "$CONFIG_JSON1" > config.json
     echo -e -n "$PORT" >> config.json
